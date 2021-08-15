956d5bfd02e9ce8450e9fdc89c00cdd01cb79c54
Siphoin <maksimka.kosolapov.01@mail.ru>
2021-08-15 10:46:18 +0500

    Update CharacterClassSettings.cs

--- Counter Blood Quest/Assets/Scripts/SO/Character/CharacterClassSettings.cs
+++ Counter Blood Quest/Assets/Scripts/SO/Character/CharacterClassSettings.cs
@@ -9,39 +9,39 @@ namespace SO.Character
         #region Fields
 
         [Header("Название")]
-        [SerializeField] private string nameClass;
+        [SerializeField] private string _nameClass;
 
         [Header("Описание")]
         [TextArea]
-        [SerializeField] private string description;
+        [SerializeField] private string _description;
 
 
         [Header("Иконка")]
-        [SerializeField] private Sprite icon;
+        [SerializeField] private Sprite _icon;
 
 
         [Header("Мини-иконка")]
-        [SerializeField] private Sprite iconMini;
+        [SerializeField] private Sprite _iconMini;
 
 
         [Header("Бонус к здоровью от базового в %")]
         [Range(-100, 100)]
-        [SerializeField] private int bonusHealthProcent = 0;
+        [SerializeField] private int _bonusHealthProcent = 0;
 
 
         [Header("Бонус к защите от базовой в %")]
         [Range(-100, 100)]
-        [SerializeField] private int bonusArmorProcent = 0;
+        [SerializeField] private int _bonusArmorProcent = 0;
 
         [Header("Бонус к урону от любого оружия в %")]
         [Range(-100, 100)]
-        [SerializeField] private int bonusDamageProcent = 0;
+        [SerializeField] private int _bonusDamageProcent = 0;
 
         [Header("Бонус к оружию любого типа активен")]
-        [SerializeField] private bool bonusDamageEnabled = false;
+        [SerializeField] private bool _bonusDamageEnabled = false;
 
         [Header("Список оружия, которые дают бонус к урону")]
-        [SerializeField] private BonusDamageWeaponSCOElement[] bonusDamageWeapons;
+        [SerializeField] private BonusDamageWeaponSCOElement[] _bonusDamageWeapons;
 
 
         #endregion
@@ -49,15 +49,15 @@ namespace SO.Character
 
         #region Properties
 
-        public string NameClass { get => nameClass; }
-        public string Description { get => description; }
-        public Sprite Icon { get => icon; }
-        public Sprite IconMini { get => iconMini; }
-        public int BonusHealthProcent { get => bonusHealthProcent; }
-        public int BonusArmorProcent { get => bonusArmorProcent; }
-        public int BonusDamageProcent { get => bonusDamageProcent; }
-        public bool BonusDamageEnabled { get => bonusDamageEnabled; }
-        public BonusDamageWeaponSCOElement[] BonusDamageWeapons { get => bonusDamageWeapons; }
+        public string NameClass => _nameClass;
+        public string Description => _description;
+        public Sprite Icon => _icon;
+        public Sprite IconMini => _iconMini;
+        public int BonusHealthProcent => _bonusHealthProcent;
+        public int BonusArmorProcent => _bonusArmorProcent;
+        public int BonusDamageProcent => _bonusDamageProcent;
+        public bool BonusDamageEnabled => _bonusDamageEnabled;
+        public BonusDamageWeaponSCOElement[] BonusDamageWeapons => _bonusDamageWeapons;
         #endregion
     }
 }
